cd75683c014dd417f8c24f8407be532e030079c3
DanSavageGames <101938430+WitheredFOT@users.noreply.github.com>
2024-10-13 17:21:47 -0700

    Update Info.java

--- src/main/java/dansavagegames/astraea/Info.java
+++ src/main/java/dansavagegames/astraea/Info.java
@@ -1,9 +1,1 @@
-package dansavagegames.astraea;
 
-public class Info {
-	
-	public static final String MODID = "astraea addons";
-	public static final String NAME = "Astraea Addons";
-	public static final String VERSION = "b0.1";
-
-}
